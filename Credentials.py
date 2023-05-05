bfe84803c6618b91223a165b2e0814c67b30ac12
Thiyagarajan V <64621469+thiyagarajan2002@users.noreply.github.com>
2023-05-05 11:31:21 +0530

    RDS

--- Credentials.py
+++ Credentials.py
@@ -1,11 +1,10 @@
 class Credentials:
     def host(self):
-        self.host="localhost"
+        self.host="project.ct2dihtq4n4l.ap-south-1.rds.amazonaws.com"
         return self.host
-
     def user(self):
-        self.user="root"
+        self.user="admin"
         return self.user
     def password(self):
-        self.password="2002"
+        self.password="trj9543460192"
         return self.password
